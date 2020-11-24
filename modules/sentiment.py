bf3c873c06a98a10e09bc91fb679b4f858cf5258
Archie Isdiningrat <archie.isdiningrat@tiket.com>
2020-11-24 12:39:17 +0700

    update sentiment logic

--- modules/sentiment.py
+++ modules/sentiment.py
@@ -1,7 +1,7 @@
 import pickle
 from tensorflow.python.keras.models import load_model
-from tensorflow.python.keras.preprocessing.text import Tokenizer
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
+from numpy import argmax
 
 MODEL = load_model('./models/sentiment.h5')
 TOKENIZER = pickle.load(open('./models/tokenizer.pkl','rb'))
@@ -9,9 +9,9 @@ CLASS_NAME = ['positive', 'neutral', 'negative']
 
 def analyze(sentence):
     tokenized = TOKENIZER.texts_to_sequences([sentence])
-    sequences = pad_sequences(tokenized, maxlen=100, padding='pre', truncating='pre')
-
-    model_result = int(MODEL.predict_classes(sequences))
+    sequences = pad_sequences(tokenized, maxlen=50, padding='pre', truncating='pre')
+    
+    model_result = argmax(MODEL.predict(sequences)[0], axis=0)
     description = CLASS_NAME[model_result]
 
     return model_result, description
---
+++ sandbox.py
@@ -0,0 +1,4 @@
+from modules import sentiment
+
+if __name__ == "__main__":
+    print(sentiment.analyze("pelayanan sangat bagus banget"))
